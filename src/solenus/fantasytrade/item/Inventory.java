401c270a77a51961ce9ba563d69078684ae4585a
CSDragon <CSDragon31@hotmail.com>
2020-06-17 00:58:00 -0500

    doing some inventoyr work

--- src/solenus/fantasytrade/item/Inventory.java
+++ src/solenus/fantasytrade/item/Inventory.java
@@ -13,7 +13,31 @@ import java.util.ArrayList;
  */
 public class Inventory 
 {
+    private double gold;
+    private ArrayList<Item> items;
     
+    public Inventory()
+    {
+        gold = 0;
+        items = new ArrayList<>();
+    }
     
+    public void addItem(Item i)
+    {
+        items.add(i);
+    }
+    
+    public void removeItem(Item i)
+    {
+        if(items.remove(i));
+    }
+    
+    /**
+     * @param g the amount of gold to add. Negative numbers remove gold.
+     */
+    public void addGold(double g)
+    {
+        gold += g;
+    }
 
 }
